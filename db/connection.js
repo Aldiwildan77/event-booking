6ccb2779f473a8f586eb88a183963d98467e14ab
Muhammad Wildan Aldiansyah <aldiwild77@gmail.com>
2019-05-31 12:31:16 +0700

    createIndex set for true

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const config = require('../config/config').dev.db
 
 mongoose.connect(`mongodb://${config.host}:${config.password}@booking-shard-00-00-x5mc8.gcp.mongodb.net:27017,booking-shard-00-01-x5mc8.gcp.mongodb.net:27017,booking-shard-00-02-x5mc8.gcp.mongodb.net:27017/${config.name}?ssl=true&replicaSet=booking-shard-0&authSource=admin&retryWrites=true`,
-  { useNewUrlParser: true })
+  { useNewUrlParser: true, useCreateIndex: true })
   .then(_ => {
     console.log("Database successfully connected")
   })
