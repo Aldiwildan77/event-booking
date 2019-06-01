c771b695b873b118089d8782c0751f191412cf2e
Muhammad Wildan Aldiansyah <aldiwild77@gmail.com>
2019-06-01 16:45:22 +0700

    refractor auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,32 +1,33 @@
 const jwt = require('jsonwebtoken')
 const config = require('../config/config').dev.auth
 
-module.exports = (req,res,next) => {
+module.exports = (req, res, next) => {
   const authHeader = req.get('Authorization')
-  if(!authHeader) {
+  if (!authHeader) {
     req.isAuth = false
     return next()
   }
 
   const token = authHeader.split(' ')[1]
-  if(!token || token === ''){
+  if (!token || token === '') {
     req.isAuth = false
     return next()
   }
 
+  let decodedToken
   try {
-    const decodedToken = jwt.verify(token, config.key)
-    if(!decodedToken){
-      req.isAuth = false
-      return next()
-    }
-
-    req.isAuth = true
-    req.userId = decodedToken.userId
-    next()
-
+    decodedToken = jwt.verify(token, config.key)
   } catch (error) {
     req.isAuth = false
-    return next(error)
+    next()
   }
+
+  if (!decodedToken) {
+    req.isAuth = false
+    return next()
+  }
+
+  req.isAuth = true
+  req.userId = decodedToken.userId
+  next()
 }
